974f37c49a3a7520e4458acd95e6c8a08a566196
s2selyn <s2selyn@gmail.com>
2025-09-24 16:56:14 +0900

    jstl

--- ActionProject/src/main/java/com/kh/el/controller/ForwardServlet.java
+++ ActionProject/src/main/java/com/kh/el/controller/ForwardServlet.java
@@ -19,6 +19,13 @@ public class ForwardServlet extends HttpServlet {
 		
 		// 얘의 목적도 jsp 만들어서 걔로 포워딩해주고싶은거죠
 		// 포워딩할 jsp 먼저 만들자
+		// 포워딩해서 forward.jsp가 응답되게하기
+		// requestDispatcher가 필요한데 이건 request 참조해서 get으로 얻을 수 있음
+		// 메소드 인자로 jsp 경로와 파일명 작성해서 전달하고
+		// 참조한 다음 forward 메소드 호출
+		// 응답데이터 니가 만들어라 하고 전달
+		request.getRequestDispatcher("/WEB-INF/views/forward.jsp")
+			   .forward(request, response);
 		
 	}
 
--- ActionProject/src/main/webapp/WEB-INF/views/forward.jsp
+++ ActionProject/src/main/webapp/WEB-INF/views/forward.jsp
@@ -8,5 +8,21 @@
 </head>
 <body>
 
+	<h1>여기는 포워드 페이지야</h1>
+	
+	<jsp:forward page="footer.jsp" />
+	<%-- 이렇게 작성하면 포워딩 페이지의 속성값이 출력된다, 뭘 해버림? 걍 포워딩 한거죠... 사실 이게 끝
+	중요한건 포워딩 했을 때의 URL을 봐야함
+	http://localhost:4000/action/forward.do
+	서블릿 매핑값이 그대로 찍혀있는데 실제로 나오는건 포워딩해놓은 footer.jsp
+	이것만 기억하면 된다!
+	--%>
+	
+	<!--
+		URL 에는 http://localhost:4000/action/forward.do 가 찍혀있음
+		
+		URL은 그대로고 화면만 바뀜
+	-->
+
 </body>
 </html>
