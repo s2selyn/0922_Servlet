d96e23dcf5ceb51e0343fed2b52c0c5ea136aa15
s2selyn <s2selyn@gmail.com>
2025-09-25 13:34:06 +0900

    select servlet

---
+++ Employee/src/main/java/com/kh/employee/controller/SelectServlet.java
@@ -0,0 +1,25 @@
+package com.kh.employee.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/select.do")
+public class SelectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public SelectServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
